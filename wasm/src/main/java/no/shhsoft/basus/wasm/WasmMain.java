b352115ff7ab84b49db476afed301a79eca895f0
Sverre H Huseby <shh@thathost.com>
2023-07-12 15:58:57 +0200

    feat: Add WASM skeleton, using fermyon's TeaVM fork

---
+++ wasm/src/main/java/no/shhsoft/basus/wasm/WasmMain.java
@@ -0,0 +1,11 @@
+package no.shhsoft.basus.wasm;
+
+/**
+ * @author <a href="mailto:shh@thathost.com">Sverre H. Huseby</a>
+ */
+public final class WasmMain {
+
+    public static void main(final String[] args) {
+    }
+
+}
