ddcb2d2ed83ea4daa31e869be1afc965eb7763a3
Sverre H Huseby <shh@thathost.com>
2023-07-12 17:20:34 +0200

    Fix host again

--- bin/sync-wasm.sh
+++ bin/sync-wasm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SSH_TARGET=pg2.thathost.com:/usr/local/share/htdocs.basus/wasm
+SSH_TARGET=basus.no:/usr/local/share/htdocs.basus/wasm
 
 rsync -av --protect-args --delete "./wasm/target/wasm/" "$SSH_TARGET/"
