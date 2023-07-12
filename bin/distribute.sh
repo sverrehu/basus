bbac3e66138e504d9f05cb971ddfba68975a08e7
Sverre H Huseby <shh@thathost.com>
2023-07-12 16:38:00 +0200

    fix: Correct host again

--- bin/distribute.sh
+++ bin/distribute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VERSION_FILE=./utils/src/main/resources/version.properties
-SSH_TARGET=pg2.thathost.com:/usr/local/share/htdocs.basus/download
+SSH_TARGET=basus.no:/usr/local/share/htdocs.basus/download
 
 if test \! -f ./bin/update-version.pl
 then
