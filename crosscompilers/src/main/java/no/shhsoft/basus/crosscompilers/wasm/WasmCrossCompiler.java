c7af946b9626f8a7982fbfc3517eff82bc904aeb
Sverre H Huseby <shh@thathost.com>
2023-11-26 16:38:16 +0100

    foo

--- crosscompilers/src/main/java/no/shhsoft/basus/crosscompilers/wasm/WasmCrossCompiler.java
+++ crosscompilers/src/main/java/no/shhsoft/basus/crosscompilers/wasm/WasmCrossCompiler.java
@@ -1,6 +1,7 @@
 package no.shhsoft.basus.crosscompilers.wasm;
 
 import no.shhsoft.basus.crosscompilers.CrossCompiler;
+import no.shhsoft.basus.language.AssignmentStatement;
 import no.shhsoft.basus.language.Statement;
 import no.shhsoft.basus.language.StatementList;
 import no.shhsoft.wasm.io.ModuleWriter;
@@ -13,6 +14,8 @@ public final class WasmCrossCompiler
 implements CrossCompiler {
 
     private Module module;
+    private IdentifierMapping functionMappings = new IdentifierMapping();
+    private IdentifierMapping globalVarsMapping = new IdentifierMapping();
 
     @Override
     public void compile(final StatementList statements, final String baseName) {
@@ -20,14 +23,26 @@ implements CrossCompiler {
             throw new RuntimeException("Method is not reentrant. Need a new WasmCrossCompiler to call compile again.");
         }
         module= new Module(1);
-        for (int q = 0; q < statements.getNumStatements(); q++) {
-            compileStatement(statements.getStatement(q));
-        }
+        compileStatements(statements);
         new ModuleWriter().writeToFile(module, baseName + ".wasm");
     }
 
-    private void compileStatement(final Statement statement) {
+    private void compileStatements(final StatementList statements) {
+        for (int q = 0; q < statements.getNumStatements(); q++) {
+            compileStatement(statements.getStatement(q));
+        }
+    }
 
+    private void compileStatement(final Statement statement) {
+        if (statement instanceof AssignmentStatement) {
+            compileAssignment((AssignmentStatement) statement);
+        } else {
+            throw new RuntimeException("Unhandled statement " + statement.getClass().getName());
+        }
+    }
+
+    private void compileAssignment(final AssignmentStatement statement) {
+        /* TODO */
     }
 
 }
