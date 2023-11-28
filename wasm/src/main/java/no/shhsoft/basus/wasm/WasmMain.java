3ccbd83768bc32ce107ae10819809218063139a0
Sverre H Huseby <shh@thathost.com>
2023-11-28 18:37:04 +0100

    use official TeaVM plugin

--- wasm/src/main/java/no/shhsoft/basus/wasm/WasmMain.java
+++ wasm/src/main/java/no/shhsoft/basus/wasm/WasmMain.java
@@ -1,12 +1,36 @@
 package no.shhsoft.basus.wasm;
 
+import no.shhsoft.basus.language.eval.runtime.BasusRunner;
+import no.shhsoft.basus.language.eval.runtime.Console;
+
 /**
  * @author <a href="mailto:shh@thathost.com">Sverre H. Huseby</a>
  */
 public final class WasmMain {
 
     public static void main(final String[] args) {
-        System.out.println("Nothing here yet.");
+        final String program = "a = 1; b = 2; c = a + b; println(c);";
+        new BasusRunner().runProgram(program, new Console() {
+            @Override
+            public void print(final String s) {
+                System.out.print(s);
+            }
+
+            @Override
+            public void println(final String s) {
+                System.out.println(s);
+            }
+
+            @Override
+            public String readln() {
+                throw new RuntimeException("Not implemented");
+            }
+
+            @Override
+            public int readKey() {
+                throw new RuntimeException("Not implemented");
+            }
+        }, null, null, null);
     }
 
 }
